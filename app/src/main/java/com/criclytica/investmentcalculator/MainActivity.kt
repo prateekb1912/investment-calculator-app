29f9a8e6143fc0a3f8ea86396c9c9e2bebb68215
prateek-ml <prateekb1912@gmail.com>
2021-06-20 13:08:10 +0530

    Add borders around textView and editText widgets

--- app/src/main/java/com/criclytica/investmentcalculator/MainActivity.kt
+++ app/src/main/java/com/criclytica/investmentcalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.criclytica.investmentcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -17,21 +18,6 @@ class MainActivity : AppCompatActivity() {
     var etRet: EditText? = null;
     var tvTotAmt: TextView? = null;
 
-    fun calcReturns() {
-        var currAge: Int = etAge?.text.toString().toInt()
-        var retAge: Int = etAgeRet?.text.toString().toInt()
-        var sipAmt: Int = etSIP?.text.toString().toInt()
-        var inflation: Int = etSIP?.text.toString().toInt()
-        var annRet: Int = etRet?.text.toString().toInt()
-
-        var numYears: Int = retAge - currAge
-        var rate: Double = annRet.toDouble()/1200
-        var totAmt: String =  (sipAmt * ((1 + rate).pow(numYears.toDouble()) - 1)* (1+rate)).toString()
-
-        tvTotAmt?.setText(totAmt)
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -43,6 +29,22 @@ class MainActivity : AppCompatActivity() {
         etInf = findViewById(R.id.etInf)
         etRet = findViewById(R.id.etRet)
         tvTotAmt = findViewById(R.id.tvTotAmt)
+
+        btnCalc.setOnClickListener(View.OnClickListener {
+            var currAge: Int = etAge?.text.toString().toInt()
+            var retAge: Int = etAgeRet?.text.toString().toInt()
+            var sipAmt: Int = etSIP?.text.toString().toInt()
+            var inflation: Int = etSIP?.text.toString().toInt()
+            var annRet: Int = etRet?.text.toString().toInt()
+
+            var numYears: Int = retAge - currAge
+            var rate: Double = annRet.toDouble()/1200
+            var totAmt: String =  (sipAmt * ((1 + rate).pow(numYears.toDouble()) - 1)* (1+rate)).toString()
+
+            tvTotAmt?.setText(totAmt)
+
+            Log.i("IC", "Function successful")
+        })
 
     }
 }
