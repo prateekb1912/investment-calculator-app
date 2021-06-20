00710f6541597c79c72a3d28cd8c2631a128291e
prateek-ml <prateekb1912@gmail.com>
2021-06-20 10:20:39 +0530

    Configure kotlin extensions

--- app/src/main/java/com/criclytica/investmentcalculator/MainActivity.kt
+++ app/src/main/java/com/criclytica/investmentcalculator/MainActivity.kt
@@ -4,8 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var etAge: EditText? = null;
+    var etAgeRet: EditText? = null;
+    var etSIP: EditText? = null;
+    var etInf: EditText? = null;
+    var etRet: EditText? = null;
+
+    fun calcReturns() {
+        Log.i("IC", "App runs successfully")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnCalc = findViewById<Button>(R.id.buttonCalc)
+        etAge = findViewById(R.id.etAge)
+        etAgeRet = findViewById(R.id.etAgeRet)
+        etSIP = findViewById(R.id.etSIP)
+        etInf = findViewById(R.id.etInf)
+        etRet = findViewById(R.id.etRet)
+
     }
 }
